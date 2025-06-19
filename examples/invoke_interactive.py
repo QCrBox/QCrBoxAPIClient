4952fd038a2316d6b8f273b5ad8aeb3f7c6c8a8b
Edward J. Parkinson <e.parkinson@soton.ac.uk>
2025-06-19 10:50:35 +0100

    update example script to include output for progress

--- examples/invoke_interactive.py
+++ examples/invoke_interactive.py
@@ -33,7 +33,7 @@ client = Client(base_url="http://127.0.0.1:11000")
 
 # Example file in QCrBox repository, but could substitite with `work.cif` which
 # 100% works w/ Olex2 and Crystal Explorer
-test_file = pathlib.Path("../robot_tests/test_data/robot_test_cif.cif").resolve()
+test_file = pathlib.Path("../robot_tests/test_data/api_client_test_cif.cif").resolve()
 
 # To upload the file to the registry, we need to create a dataset and attach the
 # file to it. Files are sent via the API as binary in a CreateDatasetBody payload
@@ -47,6 +47,8 @@ upload_payload = CreateDatasetBody(file)
 response = create_dataset.sync(client=client, body=upload_payload)
 if isinstance(response, QCrBoxErrorResponse):
     raise TypeError("Failed to upload file")
+else:
+    print("Created dataset:", response)
 
 # The response returns the created object in payload.datasets[0]. Note that this
 # doesn't contain any of the file's binary data and instead contains metadata
@@ -64,17 +66,28 @@ create_session = CreateInteractiveSession("olex2", "1.5-alpha", arguments)
 response = create_interactive_session_with_arguments.sync(client=client, body=create_session)
 if isinstance(response, QCrBoxErrorResponse):
     raise TypeError("Failed to start interactive session")
+else:
+    print("Created interactive session:", response)
 
 # The response from this endpoint is slightly different as it returns a reference
 # to the create object, rather than the object iself. The payload contains the
 # interactive_session_id which is also the calcualtion id of the interactive session
 session_id = response.payload.interactive_session_id
+print("Session ID:", session_id)
 
 # We can close the interactive session now. But we should sleep for a little while
 # so things have time to registry in all the databases and the run command can
 # execute
 time.sleep(3)
-close_interactive_session.sync(client=client, id=session_id)
+response = close_interactive_session.sync(client=client, id=session_id)
+if isinstance(response, QCrBoxErrorResponse):
+    raise TypeError("Failed to close interactive session")
+else:
+    print("Closed interactive session:", response)
 
 # Delete the dataset afterward, because we are just using this for test purposes
-delete_dataset_by_id.sync(id=dataset_id, client=client)
+response = delete_dataset_by_id.sync(id=dataset_id, client=client)
+if isinstance(response, QCrBoxErrorResponse):
+    raise TypeError("Failed to delete test dataset")
+else:
+    print("Deleted test dataset")
